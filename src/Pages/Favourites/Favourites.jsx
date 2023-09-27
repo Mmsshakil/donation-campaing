cf024af67ff3cfc8299d11641f23ce68f203c7d5
Mmsshakil <softhandshakil@gmail.com>
2023-09-27 18:33:34 +0600

    see more button solved

--- src/Pages/Favourites/Favourites.jsx
+++ src/Pages/Favourites/Favourites.jsx
@@ -1,54 +1,50 @@
 import { useEffect, useState } from "react";
 import PhoneDonationCard from "../Phone/PhoneDonationCard";
 
-
 const Favourites = () => {
+  const [favourites, setFavourites] = useState([]);
+  const [noFound, setNoFound] = useState(false);
+  const [isShow, setIsShow] = useState(false);
 
-    const [favourites, setFavourites] = useState([]);
-    const [noFound, setNoFound] = useState(false);
-    const [isShow, setIsShow] = useState(false);
+  useEffect(() => {
+    const favouriteItems = JSON.parse(localStorage.getItem("favourites"));
+    if (favouriteItems) {
+      setFavourites(favouriteItems);
+    } else {
+      setNoFound("No data found");
+    }
+  }, []);
 
-    useEffect(() => {
-        const favouriteItems = JSON.parse(localStorage.getItem('favourites'));
-        if (favouriteItems) {
-            setFavourites(favouriteItems);
-        }
-        else {
-            setNoFound('No data found');
-        }
-    }, [])
+  return (
+    <div>
+      {noFound ? (
+        <p className="flex justify-center items-center h-[80vh]">{noFound}</p>
+      ) : (
+        <div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-10 mx-2 lg:mx-0 mb-10">
+            {isShow
+              ? favourites.length &&
+                favourites?.map((phone) => (
+                  <PhoneDonationCard key={phone.id} phone={phone}></PhoneDonationCard>
+                ))
+              : favourites.length &&
+                favourites?.slice(0, 4).map((phone) => (
+                  <PhoneDonationCard key={phone.id} phone={phone}></PhoneDonationCard>
+                ))}
+          </div>
 
-    // console.log(favourites);
-    // console.log(isShow);
-
-    return <div>
-        {
-            noFound ? <p className="flex justify-center items-center h-[80vh]">{noFound}</p>
-                :
-
-                <div>
-
-                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-10 mx-2 lg:mx-0 mb-10">
-                        {
-                            isShow ? favourites.length && favourites?.map(phone => <PhoneDonationCard key={phone.id} phone={phone} ></PhoneDonationCard>)
-                                :
-                                favourites.length && favourites?.slice(0, 4).map(phone => <PhoneDonationCard key={phone.id} phone={phone} ></PhoneDonationCard>)
-                        }
-                    </div>
-
-                    {!isShow && (
-                        <button
-                            onClick={() => setIsShow(true)}
-                            className="font-semibold text-center bg-green-800 text-white p-3 rounded-md mx-auto block mb-10"
-                        >
-                            See More
-                        </button>
-                    )}
-
-
-
-
-                </div>}</div>;
+          {!isShow && favourites.length > 4 && (
+            <button
+              onClick={() => setIsShow(true)}
+              className="font-semibold text-center bg-green-800 text-white p-3 rounded-md mx-auto block mb-10"
+            >
+              See More
+            </button>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Favourites;
