aa13ff6536c74f67e4ebe9d1c24aced979d7f7ac
Mmsshakil <softhandshakil@gmail.com>
2023-09-27 17:03:41 +0600

    statiscts added but have some problem

--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -2,8 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import Favourites from "../Pages/Favourites/Favourites";
-import Statistics from "../Pages/Statistics/Statistics";
+// import Statistics from "../Pages/Statistics/Statistics";
 import Phone from "../Pages/Phone/Phone";
+
+import Statistics from "../Pages/Statistics/Statictics";
 
 
 const myCreatedRoute = createBrowserRouter([
@@ -14,7 +16,7 @@ const myCreatedRoute = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
-                loader: ()=> fetch("../../public/phones.json")
+                loader: ()=> fetch("./phones.json")
             },
             {
                 path: '/favourites',
@@ -27,7 +29,7 @@ const myCreatedRoute = createBrowserRouter([
             {
                 path: '/phones/:id',
                 element: <Phone></Phone>,
-                loader: ()=> fetch("../../public/phones.json")
+                loader: ()=> fetch("./phones.json")
             }
         ]
         
